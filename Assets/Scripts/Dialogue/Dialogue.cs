8f65e2dab2a72ae4a116f73da0284c72bc828145
bugraatabek <bugratabek@gmail.com>
2023-01-10 18:40:47 +0300

    5) Dialogue Editor/Dialogue(Update)
    Update Dialogue : on Awake if the Dialogue list is empty, Dialogue scriptableObject Automatically adds a default node to the list.
    Update DialogueEditor : Foreach node.text  in the selectedDialogue now shows the  texts in the window.

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -7,6 +7,21 @@ namespace RPG.Dialogue
     [CreateAssetMenu(fileName = "New Dialogue", menuName = "RPG/Dialogue")]
     public class Dialogue : ScriptableObject
     {
-        [SerializeField] DialogueNode[] nodes;
+        [SerializeField] List<DialogueNode> nodes = new List<DialogueNode>();
+        
+#if UNITY_EDITOR
+        private void Awake() 
+        {
+            if(nodes.Count == 0)
+            {
+                var defaultNode = new DialogueNode();
+                nodes.Add(defaultNode);
+            }
+        }
+#endif
+        public IEnumerable<DialogueNode> GetAllNodes()
+        {
+            return nodes;
+        }    
     }
 }
--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -52,7 +52,10 @@ namespace RPG.Dialogue.Editor
             }
             else
             {
-                EditorGUILayout.LabelField(selectedDialogue.name);
+                foreach (DialogueNode node in selectedDialogue.GetAllNodes())
+                {
+                    EditorGUILayout.LabelField(node.text);
+                }
             }
         }
     }
