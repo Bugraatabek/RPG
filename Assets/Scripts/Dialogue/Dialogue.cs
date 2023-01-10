7d0e03bfd2d95d471ae9f5ef2cf875bb970c20eb
bugraatabek <bugratabek@gmail.com>
2023-01-10 16:57:07 +0300

    3) Dialogue Editor. (Update)
    When clicked on an object typeof Dialogue. The editor goes ahead and opens up the DialogueEditor window.

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace RPG.Dialogue
 {
     [CreateAssetMenu(fileName = "New Dialogue", menuName = "RPG/Dialogue")]
--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
-namespace Rpg.Dialogue.Editor
+
+namespace RPG.Dialogue.Editor
 {
-    
     public class DialogueEditor : EditorWindow
     {
+        
         [MenuItem("Window/Dialogue Editor")]
         public static void ShowEditorWindow()
         {
             GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
+        }
+
+        [OnOpenAssetAttribute(1)]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+            if(dialogue != null)
+            {
+                ShowEditorWindow();
+                return true;
+            }
+            return false;
         }
     }
 }
